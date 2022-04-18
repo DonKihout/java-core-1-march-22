13a0d0b2dacfc4f3b903ffb5a136cc20cd7bed9a
DonKihout <Vadim4ik.2017@yandex.ru>
2022-04-18 16:33:18 +0500

    Lesson8 HomeWork

---
+++ src/main/java/ru/gb/java_core1/l8_Homework/GameStarter.java
@@ -0,0 +1,12 @@
+package ru.gb.java_core1.l8_Homework;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class GameStarter {
+    public static void main(String[] args) throws IOException {
+        new GameWindow();
+    }
+}
